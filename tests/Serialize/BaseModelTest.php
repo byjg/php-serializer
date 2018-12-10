c5d37ab675185da865e68ae2293caca314e8f80d
Joao Gilberto Magalhaes <joao@byjg.com.br>
2018-12-09 20:21:50 -0600

    Fix Unit Test

--- tests/Serialize/BaseModelTest.php
+++ tests/Serialize/BaseModelTest.php
@@ -8,26 +8,6 @@ use Tests\Sample\SampleModel;
 
 class BaseModelTest extends TestCase
 {
-
-    /**
-     * Sets up the fixture, for example, opens a network connection.
-     * This method is called before a test is executed.
-     */
-    protected function setUp()
-    {
-
-    }
-
-    /**
-     * Tears down the fixture, for example, closes a network connection.
-     * This method is called after a test is executed.
-     */
-    protected function tearDown()
-    {
-        
-    }
-
-
     public function testBindFromObject()
     {
         $model = new ModelGetter(10, 'Testing');
