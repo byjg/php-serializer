d59c4bbd05b06b30d7aaf566f3fb08a794135dea
Joao Gilberto Magalhaes <joao@byjg.com.br>
2025-08-23 14:02:04 -0400

    Redcuce complexity of function

--- src/ObjectCopy.php
+++ src/ObjectCopy.php
@@ -25,40 +25,60 @@ final class ObjectCopy
         $sourceArray = Serialize::from($source)
             ->withStopAtFirstLevel()
             ->parseAttributes(
-                function ($attribute, $value, $keyName, $propertyName) use ($propertyHandler, $target, $propNameLower, $source) {
-                    // ----------------------------------------------
-                    // Extract the target name
-                    $targetName = $propertyName;
-                    if (!is_null($propertyHandler)) {
-                        $targetName = $propertyHandler->mapName($propertyName);
-                        // Pass the full source instance to allow property handler to access other properties
-                        $value = $propertyHandler->transformValue($propertyName, $targetName, $value, $source);
-                    }
-
-                    // ----------------------------------------------
-                    // Set the value to the target
-                    if (method_exists($target, 'set' . $targetName)) {
-                        $target->{'set' . $targetName}($value);
-                    } elseif (isset($target->{$targetName}) || $target instanceof stdClass) {
-                        $target->{$targetName} = $value;
-                    } else {
-                        // Check if source property have property case name different from target
-                        $className = get_class($target);
-                        if (!isset($propNameLower[$className])) {
-                            $propNameLower[$className] = [];
-
-                            $classVars = get_class_vars($className);
-                            foreach ($classVars as $varKey => $varValue) {
-                                $propNameLower[$className][strtolower($varKey)] = $varKey;
-                            }
-                        }
-
-                        $propLower = strtolower($targetName);
-                        if (isset($propNameLower[$className][$propLower])) {
-                            $target->{$propNameLower[$className][$propLower]} = $value;
-                        }
-                    }
+                function ($attribute, $value, $keyName, $propertyName) use ($propertyHandler, $target, &$propNameLower, $source) {
+                    self::applyAttribute($attribute, $value, $keyName, $propertyName, $propertyHandler, $target, $propNameLower, $source);
                 }
             );
+    }
+
+    /**
+     * @param mixed $attribute
+     * @param mixed $value
+     * @param mixed $keyName
+     * @param string $propertyName
+     * @param PropertyHandlerInterface|null $propertyHandler
+     * @param object|array $target
+     * @param array $propNameLower
+     * @param object|array $source
+     * @return void
+     */
+    private static function applyAttribute(mixed $attribute, mixed $value, mixed $keyName, string $propertyName, ?PropertyHandlerInterface $propertyHandler, object|array $target, array &$propNameLower, object|array $source): void
+    {
+        // ----------------------------------------------
+        // Extract the target name
+        $targetName = $propertyName;
+        if (!is_null($propertyHandler)) {
+            $targetName = $propertyHandler->mapName($propertyName);
+            // Pass the full source instance to allow property handler to access other properties
+            $value = $propertyHandler->transformValue($propertyName, $targetName, $value, $source);
+        }
+
+        // ----------------------------------------------
+        // Set the value to the target
+        if (method_exists($target, 'set' . $targetName)) {
+            $target->{'set' . $targetName}($value);
+            return;
+        }
+
+        if (isset($target->{$targetName}) || $target instanceof stdClass) {
+            $target->{$targetName} = $value;
+            return;
+        }
+
+        // Check if source property have property case name different from target
+        $className = get_class($target);
+        if (!isset($propNameLower[$className])) {
+            $propNameLower[$className] = [];
+
+            $classVars = get_class_vars($className);
+            foreach ($classVars as $varKey => $varValue) {
+                $propNameLower[$className][strtolower($varKey)] = $varKey;
+            }
+        }
+
+        $propLower = strtolower($targetName);
+        if (isset($propNameLower[$className][$propLower])) {
+            $target->{$propNameLower[$className][$propLower]} = $value;
+        }
     }
 }
